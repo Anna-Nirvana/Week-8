b1920f46f0f5a134b14381cf947777f2102cb1e3
Anna <annmarcam@gmail.com>
2021-03-25 17:10:19 -0700

    complete code tutorial

--- script.js
+++ script.js
@@ -16,12 +16,67 @@ const containerG = svg.append('g').classed('container', true);
 d3.json('data/flare-processed.json')
 .then(function(data) {
 
+let hierarchy = d3.hierarchy(data);
+console.log(hierarchy);
 
+let bilinks = link2way(hierarchy);
+
+let layout = d3.cluster()
+    .size([2*Math.PI, size.w/2 - 100]) // angular position on circle using radians to later rotate text
+    (bilinks);
+console.log(layout);
+
+let node = svg.append('g')
+    .attr('font-family', 'sans-serif')
+    .attr('font-size', 10)
+    .selectAll('g')
+    .data(layout.leaves()) // only drawing the leaf nodes
+    .join('g')
+    .attr('transform', d => `rotate(${d.x * 180 / Math.PI - 90}) translate(${d.y},0)`)
+    .append('text')
+    .attr('text-anchor', d => d.x < Math.PI ? 'start' : 'end')
+    // TRY: try to change 'end' to 'start' and observe the effects
+    .attr('transform', d => d.x >= Math.PI ? 'rotate(180)' : null)
+    .text(d => d.data.name);
+
+let line = d3.lineRadial()
+    .curve(d3.curveBundle.beta(0.85))  
+    .radius(d => d.y)
+    .angle(d => d.x);
     
+    let link = svg.append('g')
+    .attr('stroke', '#aaa')
+    .attr('fill', 'none')
+    .selectAll('path')
+    .data(layout.leaves().flatMap(leaf => leaf.outgoing))
+    .join('path')
+    .style('mix-blend-mode', 'mulitply')
+    .attr('d', d => {
+        let node = d[0];
+        let connectedNode = d[1];
+        return line(node.path(connectedNode));
+    });
 });
 
 function link2way(root) {
-    
+    let obj = {};
+    root.leaves().forEach(d => {
+        let nodeId= id(d);
+        obj[nodeId] = d;
+    });
+
+    for (const d of root.leaves()) {
+        d.incoming=[];
+        d.outgoing = d.data.imports.map(e => [d, obj[e]]);
+    }
+
+    for (const d of root.leaves()) {
+        for (const o of d.outgoing) {
+            o[1].incoming.push(o);
+        }
+    }
+
+    return root;
 }
 
 function id(node) {
@@ -32,3 +87,4 @@ function id(node) {
     // refer: https://observablehq.com/@d3/tidy-tree
     return `${node.parent ? id(node.parent) + '.' : ''}${node.data.name}`;
 }
+
